c5712ad3f6bd25e974500be6baedb5b501c3e9b4
Jonathan Krall <jonathan.krall@lexicon.se>
2024-11-13 16:46:37 +0100

    Added functionality to see attending members in details.
    v0.2 Done

--- GymBooking/Controllers/GymClassesController.cs
+++ GymBooking/Controllers/GymClassesController.cs
@@ -37,14 +37,17 @@ namespace GymBooking.Controllers
                 return NotFound();
             }
 
-            var gymClass = await _context.GymClasses
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (gymClass == null)
+            var gymClassWithAttendees = await _context.GymClasses
+                .Where(g=>g.Id == id)
+                .Include(c=>c.AttendingMembers)
+                .ThenInclude(u=>u.User).FirstOrDefaultAsync();
+
+            if (gymClassWithAttendees == null)
             {
                 return NotFound();
             }
 
-            return View(gymClass);
+            return View(gymClassWithAttendees);
         }
 
         // GET: GymClasses/Create
