3428d18d07e1f2c73018ec7a82d7bb529c00f706
Jonathan Krall <jonathan.krall@lexicon.se>
2024-11-14 10:07:50 +0100

    Seeded in AdminRole

--- GymBooking/Program.cs
+++ GymBooking/Program.cs
@@ -1,4 +1,5 @@
 using GymBooking.Data;
+using GymBooking.Extensions;
 using GymBooking.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -7,7 +8,7 @@ namespace GymBooking
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -35,6 +36,8 @@ namespace GymBooking
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            await app.SeedDataAsync(); 
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
