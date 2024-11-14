02eaebce7e59c19db7b8c94923405aa50523c171
Jonathan Krall <jonathan.krall@lexicon.se>
2024-11-14 13:34:24 +0100

    Updated ApplicationUser

--- GymBooking/Models/ApplicationUser.cs
+++ GymBooking/Models/ApplicationUser.cs
@@ -4,6 +4,12 @@ namespace GymBooking.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        public DateTime TimeOfRegistration { get; set; }
 
         public ICollection<ApplicationUserGymClass> AttendedClasses { get; set; }
     }
